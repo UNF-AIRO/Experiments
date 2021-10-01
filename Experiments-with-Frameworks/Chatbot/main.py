4f71a52471d8dc143c5204e8649116b2e44593a6
Andreas Ink <andreasink17@gmail.com>
2021-10-01 00:44:53 -0400

    Added nltk download

--- Experiments-with-Frameworks/Chatbot/main.py
+++ Experiments-with-Frameworks/Chatbot/main.py
@@ -4,6 +4,8 @@ from keras.models import load_model
 model = load_model('chatbot_model.h5')
 import json
 import random
+import nltk
+nltk.download('punkt')
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
