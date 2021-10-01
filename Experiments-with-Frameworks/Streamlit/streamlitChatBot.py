bde556525a7511ba6d1d867b6559595b5bc27b27
Andreas Ink <andreasink17@gmail.com>
2021-09-30 21:30:44 -0400

    Fixed issue with streamlit deployment

--- Experiments-with-Frameworks/Streamlit/streamlitChatBot.py
+++ Experiments-with-Frameworks/Streamlit/streamlitChatBot.py
@@ -13,7 +13,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-local_css("style.css")
+local_css("./style.css")
 if 'count' not in st.session_state:
 	st.session_state.count = 0
 
