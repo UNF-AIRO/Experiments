2afcbf15121280dff94b38d85a68e7b333447c4f
Andreas Ink <andreasink17@gmail.com>
2021-10-03 18:16:11 -0400

    Fixing streamlit issues

--- Experiments-with-Frameworks/Chatbot/main.py
+++ Experiments-with-Frameworks/Chatbot/main.py
@@ -18,7 +18,6 @@ model = load_model('chatbot_model.h5')
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
-newMessage = ""
 oldMessage = "hi!"
 
 
