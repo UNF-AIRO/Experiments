4a81327d0cc1106c60e43b804154d3cae2a6f525
Andreas Ink <andreasink17@gmail.com>
2021-10-03 18:04:48 -0400

    Fixing message issues

--- Experiments-with-Frameworks/Chatbot/main.py
+++ Experiments-with-Frameworks/Chatbot/main.py
@@ -71,18 +71,18 @@ start = st.button("Chat")
 if start:
     st.session_state.count += 1
 if  st.session_state.count > 0:
-    if newMessage != oldMessage:
-       
+    
+        
         message = Message(isAi=False, text= newMessage)
         st.session_state.messages.append(message)
         for message in st.session_state.messages:
             chatBubble(message)
-        
+
         aiMessage = Message(isAi=True, text=cb.chatbot_response(newMessage))
         st.session_state.messages.append(aiMessage)
     # Set newMessage to a text field
-    newMessage = st.text_input("Send Message")
-    oldMessage = newMessage
+        newMessage = st.text_input("Send Message")
+        oldMessage = newMessage
 
     # Initalize button
    
