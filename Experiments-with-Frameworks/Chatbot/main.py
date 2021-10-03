1c880ed23f2df632b9d5aec1ebc1fe4bd56b03f8
Andreas Ink <andreasink17@gmail.com>
2021-10-03 18:00:29 -0400

    Fixing chat issue

--- Experiments-with-Frameworks/Chatbot/main.py
+++ Experiments-with-Frameworks/Chatbot/main.py
@@ -19,7 +19,7 @@ intents = json.loads(open('intents.json').read())
 words = pickle.load(open('words.pkl','rb'))
 classes = pickle.load(open('classes.pkl','rb'))
 newMessage = ""
-oldMessage = ""
+oldMessage = "hi!"
 
 
 # Message class defined in Pydantic
