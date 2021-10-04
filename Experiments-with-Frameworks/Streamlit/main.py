89f1f2c53211aff1fa0533d1c9dd75626d7d6e96
Senal Bulumulle <senal.bulumulle@gmail.com>
2021-10-04 09:10:52 -0400

    Commented the Code in main.py in the Streamlit Folder in Experiments-with-Frameworks Folder.
    Commented the Code in main.py in the Streamlit Folder in Experiments-with-Frameworks Folder.

--- Experiments-with-Frameworks/Streamlit/main.py
+++ Experiments-with-Frameworks/Streamlit/main.py
@@ -1,8 +1,17 @@
 import streamlit as st
+
 from pydantic import BaseModel
+
 from typing import List
+
 import os
+
+
+
+
+
 # Message class defined in Pydantic
+
 class Message(BaseModel):
     isAi: bool
     text: str
@@ -10,19 +19,40 @@ newMessage = ""
 oldMessage = "1"
 st.set_page_config(page_title='AiRO', page_icon='🧠', layout="wide")
 
+
+
+
+
+
+# Make a function called local_css that will create a custom css 
+# stylesheet
+
+
 def local_css():
    with open("style.css") as f:
     
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+
+
+## Call local_css function
 local_css()
+
+
 
 if 'count' not in st.session_state:
 	st.session_state.count = 0
 
+
+
+
 if 'messages' not in st.session_state:
 	st.session_state.messages = list()
 
+
+
+
+## Make a function called chatBubble
 def chatBubble(message):
     col1, col2, col3 = st.columns(3)
     if message.isAi:
@@ -32,12 +62,25 @@ def chatBubble(message):
         with col2:
             st.markdown(f'<div class="chat"><div class="mine messages"><div class="message last">{message.text}</div></div> <div class="mine messages">', unsafe_allow_html=True)
 
+
+
+## Display the AIRO Logo
 st.image("AiRO.png")
+
+## Display the Title 
 st.title("Artificial Intelligence Research Organization")
+
+
+## Display the Subtitle
 
 st.subheader("Chat about UNF...")
 
+
+# Display the chat button
+
 start = st.button("Chat")
+
+
 
 if start:
     st.session_state.count += 1
