78b8f3f72decb6296eea5fd33c061b00d1136e28
senalbulumulle <senal.bulumulle@gmail.com>
2022-01-31 21:07:01 -0500

    Added a Installation Script in [Experiments/Experiments-with-Frameworks/Streamlit]

---
+++ Experiments-with-Frameworks/Streamlit/install.sh
@@ -0,0 +1,10 @@
+#################################################################
+#	install.sh
+#	This script allows the installation of dependencies 
+#	to run the program
+#
+#################################################################
+
+
+
+sudo -H pip3 install streamlit
