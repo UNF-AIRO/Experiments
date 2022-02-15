facb43ec095cc1b2023d1a3fbd9cf71f57da94a0
senalbulumulle <senal.bulumulle@gmail.com>
2022-02-15 17:17:09 -0500

    I Fixed an Issue in the [Experiments-with-Frameworks/Chatbot]

---
+++ Experiments-with-Frameworks/Chatbot/install.sh
@@ -0,0 +1,15 @@
+#################################################################
+#	install.sh
+#	This script allows the installation of dependencies 
+#	to run the program
+#
+#################################################################
+
+
+
+pip3 install streamlit
+pip3 install numpy
+pip3 install nltk
+pip3 install keras
+pip3 install tensorflow
+pip3 install pydantic
