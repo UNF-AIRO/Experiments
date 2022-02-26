ec54f269c0fc44f6cced517cb8ab911498287057
senalbulumulle <senal.bulumulle@gmail.com>
2022-02-25 20:53:34 -0500

    Moved files to [Experiments-with-Frameworks/Chatbot]

--- tempTwo.sh
+++ tempTwo.sh
@@ -7,10 +7,10 @@
 
 cd Experiments-with-Frameworks/Chatbot
 
-mv style.css ../../../
-mv chatbot_model.h5 ../../../
-mv classes.pkl ../../../
-mv words.pkl ../../../
-mv intents.json ../../../
-mv Speech-Recognition ../../../
-mv AiRO.png ../../../
+mv style.css ../../
+mv chatbot_model.h5 ../../
+mv classes.pkl ../../
+mv words.pkl ../../
+mv intents.json ../../
+mv Speech-Recognition ../../
+mv AiRO.png ../../
