b344466bf8e54228e38685af3ed1265a615f1e52
senalbulumulle <senal.bulumulle@gmail.com>
2022-02-25 19:28:20 -0500

    Added temp files

---
+++ tempOne.sh
@@ -0,0 +1,15 @@
+####################################################
+#	tempOne.sh
+#	Moves files to Experiments-with-Frameworks/Chatbot
+#
+#
+####################################################
+
+
+mv style.css Experiments-with-Frameworks/Chatbot
+mv chatbot_model.h5 Experiments-with-Frameworks/Chatbot
+mv classes,pkl Experiments-with-Frameworks/Chatbot
+mv words.pkl Experiments-with-Frameworks/Chatbot
+mv intents.json Experiments-with-Frameworks/Chatbot
+mv Speech-Recognition Experiments-with-Frameworks/Chatbot
+mv AiRO.png Experiments-with-Frameworks/Chatbot
