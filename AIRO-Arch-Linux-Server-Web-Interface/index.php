5bef1578e783558881d8686fa556f5c22889a1eb
senalbulumulle <senal.bulumulle@gmail.com>
2022-03-09 11:47:01 -0500

    Added Files in AIRO-Arch-Linux-Server-Web-Interface

---
+++ AIRO-Arch-Linux-Server-Web-Interface/index.php
@@ -0,0 +1,9 @@
+<!-- 
+    ******************************************
+    *       AIRO-Arch-Linux-Server-Web-Interface
+    *
+    *
+    ******************************************
+
+-->
+<link rel="stylesheet" href="FOIL-UI-Framework-Library/FOIL-UI.css">
