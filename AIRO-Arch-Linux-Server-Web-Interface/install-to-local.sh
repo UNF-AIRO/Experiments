0a2186fb30c561b73e8060fb2a2382f233f56c42
senalbulumulle <senal.bulumulle@gmail.com>
2022-03-10 17:31:56 -0500

    Remove file install-to-install.sh

--- AIRO-Arch-Linux-Server-Web-Interface/install-to-local.sh
+++
@@ -1,7 +0,0 @@
-cd /home/
-mkdir AIRO-Arch-Linux-Server-Web-Interface
-cd
-cd Experiments/AIRO-Arch-Linux-Server-Web-Interface
-cp index.php /home/AIRO-Arch-Linux-Server-Web-Interface
-cp -R src /home/AIRO-Arch-Linux-Server-Web-Interface
-cp README.txt /home/AIRO-Arch-Linux-Server-Web-Interface
