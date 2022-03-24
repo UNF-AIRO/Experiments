d7c998c860be1ebd5d12c819ae46ac6b12bf4f21
senalbulumulle <senal.bulumulle@gmail.com>
2022-03-24 18:47:31 -0400

    Added System_Files from FOIL-Home

---
+++ AIRO-Arch-Linux-Server-Web-Interface/System_Files/php/system-applications-menu.php
@@ -0,0 +1,11 @@
+<div id="Applications" class="activate">
+    <div class="options">
+        <a class="close" href="#">&times;</a>
+        <h1> Applications </h1>
+        <ul></ul>
+        <a href="../../APPS/Calculator/index.php" class="button">Calculator</a>
+        <a href="../../APPS/AnalogClock/index.php" class="button">Analog Clock</a>
+        <a href="../../APPS/Settings/index.php" class="button">Settings</a>
+        <div class="separate"></div>
+    </div>
+</div>
