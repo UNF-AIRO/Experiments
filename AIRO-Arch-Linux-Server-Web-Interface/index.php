58b264b3af90d056f784360e2541e2e8cd4d505b
senalbulumulle <senal.bulumulle@gmail.com>
2022-03-24 15:09:07 -0400

    Converted to php syntax in index.php

--- AIRO-Arch-Linux-Server-Web-Interface/index.php
+++ AIRO-Arch-Linux-Server-Web-Interface/index.php
@@ -1,23 +1,28 @@
-<!-- 
-    ******************************************
-    *       AIRO-Arch-Linux-Server-Web-Interface
-    *
-    *
-    ******************************************
+<?php
 
--->
-
-<link rel="stylesheet" href="src/FOIL-UI-Framework-Library/FOIL-UI.css">
+class mainMenu {
+        const INCLUDE_USER_INTERFACES = "
+                                        <link href='stylesheet' href='src/FOIL-UI-Framework-Library/FOIL-UI.css'>
+                                        ";
 
 
-<div class="FOIL-Font-Styles--sans-serif">
 
-<div class="FOIL-Navigation-Bar">
-	 <h1> Menu </h1>
-</div>
+        const SUB_HEADER_CONTENTS = "
+                                        <link rel='stylesheet' href='src/FOIL-UI-Framework-Library/FOIL-UI.css'>
+                                        <div class='FOIL-Font-Styles--sans-serif'>
+                                        <div class='FOIL-Navigation-Bar'>
+                                       	<h1> Menu </h1>
+                                        </div>
+                                        <a href='shutdown.php' class='FOIL-Button color-is-red'>SHUTDOWN </a>
+                                        <a href='restart.php' class='FOIL-Button color-is-red'>RESTART</a>
+                                        </div>
+                                    ";
 
 
-<a href="#" class="FOIL-Button color-is-red">SHUTDOWN </a>
-<a href="#" class="FOIL-Button color-is-red">RESTART</a>
+        
+}
 
-</div>
+echo mainMenu::INCLUDE_USER_INTERFACES;                 // ENABLE FOIL-UI
+echo mainMenu::SUB_HEADER_CONTENTS;                     // ENABLE SUB HEADER CONTENTS
+
+?>
